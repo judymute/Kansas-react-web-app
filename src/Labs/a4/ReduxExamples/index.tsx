9013157c3053ec72972249f11341e652ebdf82c5
judymute <julie.lam1181@gmail.com>
2024-04-22 20:30:11 -0400

    imported todos

--- src/Labs/a4/ReduxExamples/index.tsx
+++ src/Labs/a4/ReduxExamples/index.tsx
@@ -2,11 +2,16 @@ import React from "react";
 import HelloRedux from "./HelloRedux";
 import CounterRedux from "./CounterRedux";
 import AddRedux from "./AddRedux";
+import TodoList from "./todos/TodoList";
+import TodoForm from "./todos/TodoForm";
 
 const ReduxExamples = () => {
   return(
     <div>
       <h2>Redux Examples</h2>
+      <TodoList/>
+      <TodoForm/>
+
       <HelloRedux/>
       <br/>
       <CounterRedux/>
